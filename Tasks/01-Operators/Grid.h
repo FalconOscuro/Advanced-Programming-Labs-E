cf56ed4fbac60dd985f9110ca67129adf5d61824
Peter Garrod <p.jlgarrod@gmail.com>
2023-04-13 16:33:47 +0100

    Finished task 1 practical

---
+++ Tasks/01-Operators/Grid.h
@@ -0,0 +1,22 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <iostream>
+
+class Grid
+{
+public:
+    Grid();
+    ~Grid();
+
+    bool LoadGrid(const char filename[]);
+    void SaveGrid(const char filename[]);
+
+    friend std::ostream& operator<<(std::ostream& os, const Grid& grid);
+    friend std::istream& operator>>(std::istream& is, Grid& grid);
+
+private:
+    int m_Grid[9][9];
+};
+
+#endif // GRID_H
---
+++ Tasks/01-Operators/Main.cpp
@@ -0,0 +1,12 @@
+#include "Grid.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Grid grid;
+    cin >> grid;
+
+    cout << grid << endl;
+}
